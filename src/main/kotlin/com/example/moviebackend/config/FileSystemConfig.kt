ec2c216e2c377a600cdec6a83b528ea86d929c5d
kyle.um(엄기훈)/kakao <pikachom@gmail.com>
2022-11-13 21:18:12 +0900

    style: remove useless bean

--- src/main/kotlin/com/example/moviebackend/config/FileSystemConfig.kt
+++ src/main/kotlin/com/example/moviebackend/config/FileSystemConfig.kt
@@ -1,6 +1,5 @@
 package com.example.moviebackend.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.File
 
@@ -11,10 +10,6 @@ class FileSystemConfig {
     }
 
     lateinit var fsMap: Map<String?, String>
-
-    @Bean(name = ["json-directory"])
-    fun fileSystem(): Map<String?, String> = this.fsMap
-
 
     fun refreshFileSystem() {
         val workingDir = System.getProperty("user.dir")
